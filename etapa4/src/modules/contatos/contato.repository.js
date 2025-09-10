4c32f2d6473a3942e4343bb07c5be19f4c148fc0
beatriz <128046453+abeazist@users.noreply.github.com>
2025-09-09 22:48:13 -0300

    separação por funcionalidades

---
+++ etapa4/src/modules/contatos/contato.repository.js
@@ -0,0 +1,40 @@
+import { randomUUID } from 'node:crypto';
+
+
+// Esta classe herda toda a responsabilidade de acesso a dados do antigo ContatoModel. O código é praticamente o mesmo, apenas realocado em um novo arquivo e classe.
+
+
+export class ContatoRepository {
+  #contatos = [];
+
+  findAll() {
+    return this.#contatos;
+  }
+
+  findById(id) {
+    return this.#contatos.find(c => c.id === id);
+  }
+
+  create(contatoData) {
+    const novoContato = { id: randomUUID(), ...contatoData };
+    this.#contatos.push(novoContato);
+    return novoContato;
+  }
+
+  update(id, contatoData) {
+ 
+    const index = this.#contatos.findIndex(c => c.id === id);
+    if (index === -1) return null;
+    
+    this.#contatos[index] = { ...this.#contatos[index], ...contatoData };
+    return this.#contatos[index];
+  }
+
+  remove(id) {
+    const index = this.#contatos.findIndex(c => c.id === id);
+    if (index === -1) return false;
+    
+    this.#contatos.splice(index, 1);
+    return true;
+  }
+}
